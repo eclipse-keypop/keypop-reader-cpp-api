c093e53561d3b070bdf105b391d863d2eeada064
jeanpierrefortune <jeanpierrefortune@users.noreply.github.com>
2024-11-20 11:12:18 +0000

    deploy: 8f0c0771e1a2599fa1b991ab33472138b2498eef

---
+++ 2.0.0-rc1/search/functions_b.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['_7ebasiccardselector_0',['~BasicCardSelector',['../classkeypop_1_1reader_1_1selection_1_1_basic_card_selector.html#a9ef6568163e1ba3cb496fa6ff789887e',1,'keypop::reader::selection::BasicCardSelector']]],
+  ['_7ecardreader_1',['~CardReader',['../classkeypop_1_1reader_1_1_card_reader.html#afc060a4b5a1ae2573acd6d37f0e4edff',1,'keypop::reader::CardReader']]],
+  ['_7ecardselectionextension_2',['~CardSelectionExtension',['../classkeypop_1_1reader_1_1selection_1_1spi_1_1_card_selection_extension.html#aad34ad45b84daee661e490997d24e084',1,'keypop::reader::selection::spi::CardSelectionExtension']]],
+  ['_7ecardselectionmanager_3',['~CardSelectionManager',['../classkeypop_1_1reader_1_1selection_1_1_card_selection_manager.html#a5945bb9e06059af90f597120375d8945',1,'keypop::reader::selection::CardSelectionManager']]],
+  ['_7econfigurablecardreader_4',['~ConfigurableCardReader',['../classkeypop_1_1reader_1_1_configurable_card_reader.html#ac4825af66d82bc58196ab841dda7deea',1,'keypop::reader::ConfigurableCardReader']]],
+  ['_7eisocardselector_5',['~IsoCardSelector',['../classkeypop_1_1reader_1_1selection_1_1_iso_card_selector.html#a7729feb4cce71b46d1d219079acd5153',1,'keypop::reader::selection::IsoCardSelector']]],
+  ['_7escheduledcardselectionsresponse_6',['~ScheduledCardSelectionsResponse',['../classkeypop_1_1reader_1_1selection_1_1_scheduled_card_selections_response.html#ae80c1060a38068684f8c803fa794e18e',1,'keypop::reader::selection::ScheduledCardSelectionsResponse']]],
+  ['_7esmartcard_7',['~SmartCard',['../classkeypop_1_1reader_1_1selection_1_1spi_1_1_smart_card.html#a4fb5ba612959ccfd977416609401ca9f',1,'keypop::reader::selection::spi::SmartCard']]]
+];
--- list_versions.md
+++ list_versions.md
@@ -1,2 +1,3 @@
 | Version | Documents |
 |:---:|---|
+| 2.0.0-rc1 | [API documentation](2.0.0-rc1) |
